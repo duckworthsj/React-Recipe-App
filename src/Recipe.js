544875abb94aedc1485373eb3b1d30c69a0b5f1f
Stephen <58013931+duckworthsj@users.noreply.github.com>
2021-03-12 17:03:17 -0500

    Add files via upload

---
+++ src/Recipe.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import style from './recipe.module.css';
+
+const Recipe = ({title,calories,image, ingredients}) => {
+    return(
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <p>{calories} calories</p>
+            <img className={style.image} src={image} alt=""/>
+            <ol>
+                {ingredients.map(ingredient =>(
+                    <li className={style.ingredient}>{ingredient.text}</li>
+                ))}
+            </ol>
+            
+        </div>
+    );
+}
+
+export default Recipe;
